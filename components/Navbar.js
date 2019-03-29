5913dde9a92740b9a7758bd0f1c01428157dfa95
hsimao <e087754958@gmail.com>
2019-03-29 21:29:09 +0800

    add: 新增Navbar組件, create Navbar component

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,12 +1,57 @@
-// import AppBar from "@material-ui/core/AppBar";
-// import Toolbar from "@material-ui/core/Toolbar";
-// import Typography from "@material-ui/core/Typography";
-// import Button from "@material-ui/core/Button";
-// import ShareOutlined from "@material-ui/icons/ShareOutlined";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import ShareOutlined from "@material-ui/icons/ShareOutlined";
 import withStyles from "@material-ui/core/styles/withStyles";
 
-const Navbar = () => {
-  return <div>Navbar</div>;
+import ActiveLink from "./ActiveLink";
+
+const Navbar = ({ classes, router, pageProps: { auth } }) => {
+  const { user = {} } = auth || {};
+  return (
+    <AppBar
+      className={classes.appBar}
+      position={router.pathname === "/" ? "fixed" : "static"}
+    >
+      <Toolbar>
+        {/*  Main title / Home Button */}
+        <ActiveLink href="/">
+          <ShareOutlined className={classes.icon} />
+        </ActiveLink>
+        <Typography
+          variant="h5"
+          component="h1"
+          color="inherit"
+          className={classes.toolbarTitle}
+        >
+          <ActiveLink href="/">NextConnect</ActiveLink>
+        </Typography>
+
+        {user._id ? (
+          // 已登入功能按鈕
+          <div>
+            <Button color="inherit">
+              <ActiveLink href="/profile">個人資料</ActiveLink>
+            </Button>
+            <Button color="inherit" variant="outlined">
+              登出
+            </Button>
+          </div>
+        ) : (
+          // 未登入功能按鈕
+          <div>
+            <Button>
+              <ActiveLink href="/signin">登入</ActiveLink>
+            </Button>
+            <Button color="inherit">
+              <ActiveLink href="/signup">註冊</ActiveLink>
+            </Button>
+          </div>
+        )}
+      </Toolbar>
+    </AppBar>
+  );
 };
 
 const styles = theme => ({
@@ -18,7 +63,8 @@ const styles = theme => ({
     flex: 1
   },
   icon: {
-    marginRight: theme.spacing.unit
+    marginRight: theme.spacing.unit,
+    marginTop: "8px"
   }
 });
 
