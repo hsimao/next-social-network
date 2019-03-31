c2ffb46e7dc01c4d691547084dbab8f2dfc36f16
hsimao <e087754958@gmail.com>
2019-03-31 18:17:38 +0800

    add: 新增個人資料頁面, profile page

--- pages/profile.js
+++ pages/profile.js
@@ -1,24 +1,93 @@
-// import Paper from "@material-ui/core/Paper";
-// import List from "@material-ui/core/List";
-// import ListItem from "@material-ui/core/ListItem";
-// import ListItemAvatar from "@material-ui/core/ListItemAvatar";
-// import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
-// import ListItemText from "@material-ui/core/ListItemText";
-// import Avatar from "@material-ui/core/Avatar";
-// import IconButton from "@material-ui/core/IconButton";
-// import Typography from "@material-ui/core/Typography";
-// import CircularProgress from "@material-ui/core/CircularProgress";
-// import Divider from "@material-ui/core/Divider";
-// import Edit from "@material-ui/icons/Edit";
+import Paper from "@material-ui/core/Paper";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
+import ListItemText from "@material-ui/core/ListItemText";
+import Avatar from "@material-ui/core/Avatar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import Divider from "@material-ui/core/Divider";
+import Edit from "@material-ui/icons/Edit";
 import withStyles from "@material-ui/core/styles/withStyles";
+import Link from "next/link";
 
 import { authInitialProps } from "../lib/auth";
+import { getUser } from "../lib/api";
 
 class Profile extends React.Component {
-  state = {};
+  state = {
+    user: null,
+    isAuth: false,
+    isLoading: true
+  };
+
+  componentDidMount() {
+    const { userId, auth } = this.props;
+    const isAuth = auth.user._id === userId;
+
+    getUser(userId).then(user => {
+      this.setState({ user, isAuth, isLoading: false });
+    });
+  }
 
   render() {
-    return <div>Profile</div>;
+    const { classes } = this.props;
+    const { isLoading, user, isAuth } = this.state;
+
+    return (
+      <Paper className={classes.root} elevation={4}>
+        <Typography
+          variant="h4"
+          component="h1"
+          align="center"
+          className={classes.title}
+          gutterBottom
+        >
+          個人資料
+        </Typography>
+        {isLoading ? (
+          <div className={classes.progressContainer}>
+            <CircularProgress
+              className={classes.progress}
+              size={55}
+              thickness={5}
+            />
+          </div>
+        ) : (
+          <List dense>
+            <ListItem>
+              <ListItemAvatar>
+                <Avatar src={user.avatar} className={classes.bigAvatar} />
+              </ListItemAvatar>
+              <ListItemText primary={user.name} secondary={user.email} />
+              {/* 本人 - 編輯按鈕 / 非本人 - 追蹤按鈕 */}
+              {isAuth ? (
+                <ListItemSecondaryAction>
+                  <Link href="/edit-profile">
+                    <a>
+                      <IconButton color="primary">
+                        <Edit />
+                      </IconButton>
+                    </a>
+                  </Link>
+                </ListItemSecondaryAction>
+              ) : (
+                <div>追蹤</div>
+              )}
+            </ListItem>
+            <Divider />
+            <ListItem>
+              <ListItemText
+                primary={user.about}
+                secondary={`註冊時間：${user.createdAt}`}
+              />
+            </ListItem>
+          </List>
+        )}
+      </Paper>
+    );
   }
 }
 
